dc53b5ba0af99b39f2c3b16ac8f284705f7555e3
Dan <91275883+DanexusS@users.noreply.github.com>
2026-08-04 22:31:45 +0300

    progression: add multi-branch early expert components

--- minecraft/kubejs/startup_scripts/expert_components.js
+++ minecraft/kubejs/startup_scripts/expert_components.js
@@ -1,14 +1,21 @@
 const EXPERT_COMPONENTS = [
-  ['mechanical_core', 'Механическое ядро', 'minecraft:item/clock'],
-  ['industrial_frame', 'Промышленная рама', 'minecraft:item/iron_ingot'],
-  ['precision_circuit', 'Прецизионная схема', 'minecraft:item/comparator'],
-  ['chemical_processor', 'Химический процессор', 'minecraft:item/ender_eye'],
-  ['bioindustrial_matrix', 'Биоиндустриальная матрица', 'minecraft:item/slime_ball'],
-  ['quantum_logic', 'Квантовая логическая матрица', 'minecraft:item/amethyst_shard'],
-  ['resonant_core', 'Резонансное энергетическое ядро', 'minecraft:item/nether_star'],
-  ['draconic_processor', 'Драконический процессор', 'minecraft:item/dragon_breath'],
-  ['transmutation_matrix', 'Матрица трансмутации', 'minecraft:item/echo_shard'],
-  ['cosmic_catalyst', 'Космический катализатор', 'minecraft:item/netherite_ingot']
+  // Early foundation. These components deliberately represent separate paths:
+  // field engineering, basic power control and material analysis.
+  ['field_engineering_kit', 'Комплект полевого инженера', 'minecraft:item/compass', 'uncommon'],
+  ['power_regulation_unit', 'Блок регулирования энергии', 'minecraft:item/redstone', 'uncommon'],
+  ['materials_analysis_matrix', 'Матрица анализа материалов', 'minecraft:item/quartz', 'uncommon'],
+  ['kinetic_interface', 'Кинетический промышленный интерфейс', 'minecraft:item/piston', 'rare'],
+
+  ['mechanical_core', 'Механическое ядро', 'minecraft:item/clock', 'rare'],
+  ['industrial_frame', 'Промышленная рама', 'minecraft:item/iron_ingot', 'rare'],
+  ['precision_circuit', 'Прецизионная схема', 'minecraft:item/comparator', 'rare'],
+  ['chemical_processor', 'Химический процессор', 'minecraft:item/ender_eye', 'rare'],
+  ['bioindustrial_matrix', 'Биоиндустриальная матрица', 'minecraft:item/slime_ball', 'rare'],
+  ['quantum_logic', 'Квантовая логическая матрица', 'minecraft:item/amethyst_shard', 'rare'],
+  ['resonant_core', 'Резонансное энергетическое ядро', 'minecraft:item/nether_star', 'rare'],
+  ['draconic_processor', 'Драконический процессор', 'minecraft:item/dragon_breath', 'rare'],
+  ['transmutation_matrix', 'Матрица трансмутации', 'minecraft:item/echo_shard', 'rare'],
+  ['cosmic_catalyst', 'Космический катализатор', 'minecraft:item/netherite_ingot', 'epic']
 ]
 
 const DIVINE_PROGRESSION_SEALS = [
@@ -28,7 +35,7 @@ StartupEvents.registry('item', event => {
     event.create(component[0])
       .displayName(component[1])
       .texture(component[2])
-      .rarity(component[0] === 'cosmic_catalyst' ? 'epic' : 'rare')
+      .rarity(component[3])
   })
 
   DIVINE_PROGRESSION_SEALS.forEach(seal => {
