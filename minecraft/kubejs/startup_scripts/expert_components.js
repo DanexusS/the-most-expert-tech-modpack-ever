0b52f1648a0dd543b949d92cbb0a6598d8f9f0d5
Dan <91275883+DanexusS@users.noreply.github.com>
2026-08-04 22:35:09 +0300

    localization: move expert item names to language assets

--- minecraft/kubejs/startup_scripts/expert_components.js
+++ minecraft/kubejs/startup_scripts/expert_components.js
@@ -1,47 +1,45 @@
 const EXPERT_COMPONENTS = [
   // Early foundation. These components deliberately represent separate paths:
   // field engineering, basic power control and material analysis.
-  ['field_engineering_kit', 'Комплект полевого инженера', 'minecraft:item/compass', 'uncommon'],
-  ['power_regulation_unit', 'Блок регулирования энергии', 'minecraft:item/redstone', 'uncommon'],
-  ['materials_analysis_matrix', 'Матрица анализа материалов', 'minecraft:item/quartz', 'uncommon'],
-  ['kinetic_interface', 'Кинетический промышленный интерфейс', 'minecraft:item/piston', 'rare'],
+  ['field_engineering_kit', 'minecraft:item/compass', 'uncommon'],
+  ['power_regulation_unit', 'minecraft:item/redstone', 'uncommon'],
+  ['materials_analysis_matrix', 'minecraft:item/quartz', 'uncommon'],
+  ['kinetic_interface', 'minecraft:item/piston', 'rare'],
 
-  ['mechanical_core', 'Механическое ядро', 'minecraft:item/clock', 'rare'],
-  ['industrial_frame', 'Промышленная рама', 'minecraft:item/iron_ingot', 'rare'],
-  ['precision_circuit', 'Прецизионная схема', 'minecraft:item/comparator', 'rare'],
-  ['chemical_processor', 'Химический процессор', 'minecraft:item/ender_eye', 'rare'],
-  ['bioindustrial_matrix', 'Биоиндустриальная матрица', 'minecraft:item/slime_ball', 'rare'],
-  ['quantum_logic', 'Квантовая логическая матрица', 'minecraft:item/amethyst_shard', 'rare'],
-  ['resonant_core', 'Резонансное энергетическое ядро', 'minecraft:item/nether_star', 'rare'],
-  ['draconic_processor', 'Драконический процессор', 'minecraft:item/dragon_breath', 'rare'],
-  ['transmutation_matrix', 'Матрица трансмутации', 'minecraft:item/echo_shard', 'rare'],
-  ['cosmic_catalyst', 'Космический катализатор', 'minecraft:item/netherite_ingot', 'epic']
+  ['mechanical_core', 'minecraft:item/clock', 'rare'],
+  ['industrial_frame', 'minecraft:item/iron_ingot', 'rare'],
+  ['precision_circuit', 'minecraft:item/comparator', 'rare'],
+  ['chemical_processor', 'minecraft:item/ender_eye', 'rare'],
+  ['bioindustrial_matrix', 'minecraft:item/slime_ball', 'rare'],
+  ['quantum_logic', 'minecraft:item/amethyst_shard', 'rare'],
+  ['resonant_core', 'minecraft:item/nether_star', 'rare'],
+  ['draconic_processor', 'minecraft:item/dragon_breath', 'rare'],
+  ['transmutation_matrix', 'minecraft:item/echo_shard', 'rare'],
+  ['cosmic_catalyst', 'minecraft:item/netherite_ingot', 'epic']
 ]
 
 const DIVINE_PROGRESSION_SEALS = [
-  ['divine_seal', 'Печать Божественного порога', 'divinerpg:item/divine_shards'],
-  ['eden_seal', 'Печать Эдема', 'divinerpg:item/eden_heart'],
-  ['wildwood_seal', 'Печать Дикого леса', 'divinerpg:item/wildwood_heart'],
-  ['apalachia_seal', 'Печать Апалачии', 'divinerpg:item/apalachia_heart'],
-  ['skythern_seal', 'Печать Скайтерна', 'divinerpg:item/skythern_heart'],
-  ['mortum_seal', 'Печать Мортума', 'divinerpg:item/mortum_heart'],
-  ['vethea_seal', 'Печать Ветеи', 'divinerpg:item/clean_pearls'],
-  ['wreck_seal', 'Печать Рэка', 'divinerpg:item/arksiane_lump'],
-  ['lunar_seal', 'Печать Леди Луны', 'divinerpg:item/everbright']
+  ['divine_seal', 'divinerpg:item/divine_shards'],
+  ['eden_seal', 'divinerpg:item/eden_heart'],
+  ['wildwood_seal', 'divinerpg:item/wildwood_heart'],
+  ['apalachia_seal', 'divinerpg:item/apalachia_heart'],
+  ['skythern_seal', 'divinerpg:item/skythern_heart'],
+  ['mortum_seal', 'divinerpg:item/mortum_heart'],
+  ['vethea_seal', 'divinerpg:item/clean_pearls'],
+  ['wreck_seal', 'divinerpg:item/arksiane_lump'],
+  ['lunar_seal', 'divinerpg:item/everbright']
 ]
 
 StartupEvents.registry('item', event => {
   EXPERT_COMPONENTS.forEach(component => {
     event.create(component[0])
-      .displayName(component[1])
-      .texture(component[2])
-      .rarity(component[3])
+      .texture(component[1])
+      .rarity(component[2])
   })
 
   DIVINE_PROGRESSION_SEALS.forEach(seal => {
     event.create(seal[0])
-      .displayName(seal[1])
-      .texture(seal[2])
+      .texture(seal[1])
       .unstackable()
       .rarity('epic')
       .containerItem(`kubejs:${seal[0]}`)
